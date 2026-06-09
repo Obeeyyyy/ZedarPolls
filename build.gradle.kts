24a7439aab498e04abfa45ed64bede69682968fa
Obey <aouna.u@proton.me>
2026-06-09 18:43:44 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("java-library")
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
---
+++ src/main/java/de/obey/zedarPolls/ZedarPolls.java
@@ -0,0 +1,11 @@
+package de.obey.zedarPolls;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZedarPolls extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+}
